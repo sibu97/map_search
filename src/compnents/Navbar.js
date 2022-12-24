2751365dbbb3c9a95016909a795bf73f59d250b6
Sibananda Nayak <nayaksibananda22@gmail.com>
2022-12-24 21:55:46 +0530

    Initial commit

--- src/compnents/Navbar.js
+++ src/compnents/Navbar.js
@@ -5,6 +5,7 @@ export class NavBar extends React.Component{
     render(){
         return(
             <div className="NavBar" >
+                <h3>Mymap Search</h3>
             </div>
         )
     }
