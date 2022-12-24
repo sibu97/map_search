48940d07f8e1fccabe8fbe6d9757b2042f63b22f
Sibananda Nayak <nayaksibananda22@gmail.com>
2022-12-24 22:05:33 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,19 +6,20 @@ import Maps from "./compnents/Map";
 function App() {
  
   return (
-    <><div style={{backgroundColor:'teal', height:"50px",width:"100vw"}}>
+    <>
+      <div style={{ backgroundColor: 'teal', height: "50px", width: "100vw" }}>
         <NavBar  />
       </div>
-    <div className="App" style={{
+     <div className="App" style={{
         display: "flex",
         border:"2px solid red",
       flexDirection: "row",
       width: "auto",
       height: "100%",
-    }} >
-      <div style={{ width: "50vw", height: "100%", }}>
-        <Maps/>
-      </div>
+       }} >
+          <div style={{ width: "50vw", height: "100%",margin:"0%" }}>
+            <Maps/>
+          </div>
       
       </div>
     </>
