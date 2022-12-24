7c223284e54d8a4c5a3075909fddf70b6454f26c
Sibananda Nayak <nayaksibananda22@gmail.com>
2022-12-24 21:39:23 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { NavBar } from './compnents/Navbar';
+import Maps from "./compnents/Map";
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><div style={{backgroundColor:'teal', height:"50px",width:"100vw"}}>
+        <NavBar  />
+      </div>
+    <div className="App" style={{
+        display: "flex",
+        border:"2px solid red",
+      flexDirection: "row",
+      width: "auto",
+      height: "100%",
+    }} >
+      <div style={{ width: "50vw", height: "100%", }}>
+        <Maps/>
+      </div>
+      
+      </div>
+    </>
   );
 }
 
---
+++ src/compnents/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class NavBar extends React.Component{
+
+    render(){
+        return(
+            <div className="NavBar" >
+            </div>
+        )
+    }
+}
