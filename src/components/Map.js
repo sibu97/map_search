0323a23d1aaa9f0ff671f387f01ecf94b6186e3c
Sibananda Nayak <nayaksibananda22@gmail.com>
2022-12-24 23:07:12 +0530

    Initial commit

--- src/components/Map.js
+++ src/components/Map.js
@@ -9,7 +9,7 @@ export default function Maps() {
       <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{width:'100%',height:'100%'}}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=GrBjBrFVzOSo0Btw4nB9"
         />
         <Marker position={position}>
           <Popup>
