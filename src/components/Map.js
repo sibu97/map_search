ab14bc1d24ed8def65a7bdb09e32f94704ece363
Sibananda Nayak <nayaksibananda22@gmail.com>
2022-12-24 23:55:17 +0530

    Initial commit

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+const icon = L.icon({
+  iconUrl: "./placeholder.png",
+  iconSize: [38, 38],
+});
 
 const position = [51.505, -0.09]
 
@@ -11,7 +17,7 @@ export default function Maps() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=GrBjBrFVzOSo0Btw4nB9"
         />
-        <Marker position={position}>
+        <Marker position={position} icon={icon}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
