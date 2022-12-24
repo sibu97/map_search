95a17b07b70d31e1b8be5c147836ac6b271cfa3d
Sibananda Nayak <nayaksibananda22@gmail.com>
2022-12-25 00:29:29 +0530

    Initial commit

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Button from "@mui/material/Button";
+
 
 const searchbox = () => {
   return (
-    <div>searchbox</div>
+    <div style={{ display: "flex",  }}>
+      <div style={{flex:1}}>
+        <OutlinedInput style={{width:"100%"}} />
+      </div>
+      <div style={{display: "flex",alignItems:"center"}}>
+      <Button variant="contained" color="primary">
+         Search
+      </Button>
+      </div>
+    </div>
+
   )
 }
 
